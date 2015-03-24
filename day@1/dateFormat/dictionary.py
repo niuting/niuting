e7c4c5ea3b84ec5e11497f57cf346ef69ea34971
“niuting” <1170146040@qq.com>
2015-03-24 10:54:49 +0800

    dateFormat

---
+++ day@1/dateFormat/dictionary.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#coding = utf-8
+
+dict = {}
+
+dict[1] = "hello"
+dict[2] = 'world'
+
+print dict
+print dict[1]
+print dict[2]
+
+tinydict = {'one':1, 'two':'two', 'three':3}
+
+print tinydict
+print tinydict.keys()
+print tinydict.values()
+print tinydict['one']
