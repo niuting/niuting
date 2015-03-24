d5c65542c927d221350b2ac314c746360432db6e
“niuting” <1170146040@qq.com>
2015-03-24 11:44:04 +0800

    dateFormat

---
+++ day@1/dateFormat/variable.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+#coding=utf-8
+
+#counter = 100
+#miles = 1000.0
+#name = "ningqi"
+#address = "aaaaa\
+#			bbbbb"
+#a=b = c = 1
+#a1, b1, c1 = 1, 2, "john"
+
+#print counter
+#print miles
+#print name 
+#print address
+
+#print a
+#print b
+#print c
+#print a1
+#print b1
+#print c1
+
+#del a
+#del a1, b1
+#print a
+#print b
+#print c
+#print a1
+#print b1
+#print c1
+
+#s = 'ilovepython' 
+#print s[1:5]
+
+str="hello world"
+
+print str
+print str[0]
+print str[1:3]
+print str[1:]
+print str[2] * 3
+print str +  "!"
+print str + '!'
+print str + """!"""
---
+++ day@1/helloWord.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+##coding=utf-8
+#-*-coding:UTF-8
+
+#print "hello World!";
+#print "你好 世界";
+
+raw_input("press the enter key to exit")#用户按下回车程序退出
